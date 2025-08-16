4d5fb78384ef93f6da40a15954ee80b5d6275701
Shreya09-maker <shreyapatil5343@gmail.com>
2025-08-16 14:32:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from tensorflow.keras.preprocessing.image import img_to_array
 # Load model
 MODEL_PATH = r"C:\Users\shrey\Downloads\New folder\tomato_model.h5"
 model = load_model(MODEL_PATH)
+FILE_ID = "https://drive.google.com/file/d/1VE7RUXKh4GupqdivjHqX_5bT6xz2z8lq/view?usp=drive_link"  
+
 
 # Class names (match your training exactly!)
 class_names = [
@@ -97,3 +99,4 @@ else:
 # Footer
 st.markdown("---")
 st.markdown("<p style='text-align: center; color: gray;'>© 2025 Tomato Leaf Disease Detector | Powered by TensorFlow & Streamlit 🍅</p>", unsafe_allow_html=True)
+
