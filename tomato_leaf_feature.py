2df4b94f673f70a2770b44cdd64b3aab29f4e927
Shreya09-maker <shreyapatil5343@gmail.com>
2025-08-16 21:47:34 +0530

    Update tomato_leaf_feature.py

--- tomato_leaf_feature.py
+++ tomato_leaf_feature.py
@@ -1,12 +1,1 @@
-import numpy as np
 
-# Load the .npy file
-features = np.load('tomato_leaf_feature.npy')
-
-# Print the type and shape of the data
-print(f"Type of data: {type(features)}")
-print(f"Shape of data: {features.shape}")
-
-# Optionally, print the first few elements to get a sense of the data
-print("First few entries:")
-print(features[:5])
